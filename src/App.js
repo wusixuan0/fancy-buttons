2dd2016e43612b13ad13d8cfe2d4f5b913c564df
Sixuan Wu <sixuan@Sixuans-iMac.local>
2023-04-09 23:08:21 -0400

    add two buttons, before refractor LightSwitchButton (keep logic that changes the state near the declaration, and not in a child component)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+import AngryButton from './components/AngryButton';
+import LightSwitchButton from './components/LightSwitchButton';
 
 function App() {
+  const [light, setLight] = useState('off');
+  const dark = (light === 'off') ? 'dark' : '';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${dark}`}>
+      <h1>Fancy Buttons!</h1>
+      <section>
+        <AngryButton />
+        <LightSwitchButton light={light} setLight={setLight}/>
+      </section>
     </div>
   );
 }
---
+++ src/components/AngryButton.js
@@ -0,0 +1,23 @@
+  import { useState } from "react";
+
+  function AngryButton(props){
+  	const [anger, setAnger] = useState(0);
+  	
+  	const handleClick = () => {
+      if (anger < 1) {
+        setAnger(anger + 0.1);
+      } else {
+        setAnger(0);
+      }
+    }
+
+    return (
+      <button onClick={handleClick} style={{ backgroundColor: `rgba(255,0,0,${anger})` }} className="AngryButton">
+        {anger < 1 && <span>Don't click me too much! </span>}
+        {anger > 1 && <span>Rawr!</span>}
+      </button>
+      
+    );
+  }
+
+  export default AngryButton;
